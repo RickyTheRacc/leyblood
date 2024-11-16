3a671a0541f85e1c2d477ea1bb0a82df5f636784
MachieCodes <rickytheracc@gmail.com>
2024-11-15 21:02:31 -0600

    Fabric Mod

---
+++ src/main/java/me/machie/leyblood/Leyblood.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 Machie
+ * All rights reserved.
+ */
+
+package me.machie.leyblood;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Leyblood implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
